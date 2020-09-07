335375ae6abd1c9d90aed247015333cbd45cb21f
Rafaela Tasso <atassorafaela@gmail.com>
2020-09-07 17:57:44 -0300

    Class 12 - Equality Operators

---
+++ classes/js/class12_equalityOperators.js
@@ -0,0 +1,12 @@
+// Strict equality
+// === -> compare value and type
+
+console.log( 1 === 1);      // true
+console.log( 1 === '1');    // false
+
+// Loose equality
+// == -> compare just value, because the software converte for the same types
+// NOT recommended  
+
+console.log( 1 == 1);      // true
+console.log( 1 == '1');    // true
