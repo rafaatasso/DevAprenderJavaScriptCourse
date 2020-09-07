10ab6170e63122a6cc04eb98df427c3abaec2d77
Rafaela Tasso <atassorafaela@gmail.com>
2020-09-07 17:33:24 -0300

    Class 10 - Arithmetics Operators

---
+++ classes/js/class10_arithmeticsOperators.js
@@ -0,0 +1,22 @@
+let salary = 100;
+
+// Add +
+// Subtract -
+// Division /
+// Multiply *
+// Exponential **
+
+console.log(salary + salary);
+console.log(salary - salary);
+console.log(salary / salary);
+console.log(salary * salary);
+console.log(2 ** 4);
+
+// Increment ++
+// Decrement --
+
+let age = 18;
+
+console.log(age++); // In this way, the computer will show the result and then increment the number
+console.log(age);
+console.log(++age); // In this way, the computer will increment the number and then show the result
