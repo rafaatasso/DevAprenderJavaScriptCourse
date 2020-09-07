4d4f6b0677e8dcfd4496dfff302a5c297f99a15f
Rafaela Tasso <atassorafaela@gmail.com>
2020-09-07 00:22:29 -0300

    Class 06 (Arrays)

---
+++ classes/js/class06_arrays.js
@@ -0,0 +1,13 @@
+// Array is a data set that can be accessed through an index
+// An index is an exact position of an element
+
+let family = [26, 45, 'Lexie', 50, 17, true, undefined];
+
+console.log(family.length);
+console.log(family);
+console.log(family[2]);
+
+/**************** HOME WORK ****************/
+
+let colleaguesName = ['Sarah', 22, 'Laguna - SC'];
+console.log(colleaguesName);
