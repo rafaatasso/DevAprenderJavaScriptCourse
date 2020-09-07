3a657dee1c02e408fbce78dbc65399d0eb6ccf3f
Rafaela Tasso <atassorafaela@gmail.com>
2020-09-07 18:39:09 -0300

    Class 13 - Ternary Operators

---
+++ classes/js/class13_ternaryOperators.js
@@ -0,0 +1,6 @@
+ // Use like if, but to better 
+ // A customer with more than 100 points is premium, if not common
+
+ let points = 200;
+ let type = points > 100 ? 'Premium' : 'Common';
+ console.log(type);
