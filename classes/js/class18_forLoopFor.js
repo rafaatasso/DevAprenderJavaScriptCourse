e7d906cd0e43582f27dcdb5a420b501db1d4a452
Rafaela Tasso <atassorafaela@gmail.com>
2020-09-08 22:13:51 -0300

    Class 18 - For / Loop For

---
+++ classes/js/class18_forLoopFor.js
@@ -0,0 +1,9 @@
+for( let i = 0; i < 5; i++ ) {
+    // console.log('I\'m learning"', i);
+
+    if( i % 2 !== 0){
+        console.log(i + ' is a Odd number!');
+    } else {
+        console.log(i + ' is a Even number!');
+    }
+}
