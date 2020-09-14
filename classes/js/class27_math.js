f17dfec0f9b2e976733a25f3f4546066896abda4
Rafaela Tasso <atassorafaela@gmail.com>
2020-09-14 17:36:47 -0300

    Class 27 - Math

---
+++ classes/js/class27_math.js
@@ -0,0 +1,27 @@
+// javascript math
+
+// Random
+const number1 = Math.random();
+const number2 = Math.random();
+
+console.log('Random: ');
+console.log(number1, number2);
+
+// Max
+const maximum = Math.max(3, 6, 8, 10);
+
+console.log('Max: ');
+console.log(maximum);
+
+// Min
+const minimum = Math.min(3, 6, 8, 10);
+
+console.log('Min: ');
+console.log(minimum);
+
+/**************** HOME WORK ****************/
+
+const round = Math.round(3.1415);
+
+console.log('Round: ');
+console.log(round);
