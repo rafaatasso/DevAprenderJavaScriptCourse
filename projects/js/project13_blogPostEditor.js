0e2d8a0a8f691a405975ad2d97177ea316577cca
Rafaela Tasso <atassorafaela@gmail.com>
2020-09-15 00:41:04 -0300

    Mini Project 13 - Blog Post Editor

---
+++ projects/js/project13_blogPostEditor.js
@@ -0,0 +1,13 @@
+let post = {
+    title: 'a',
+    message: 'a',
+    author: 'a',
+    views: 10,
+    comments: [
+        { author: 'a', message: 'b' },
+        { author: 'a', message: 'b' }
+    ],
+    isOnline: true
+};
+
+console.log(post);
