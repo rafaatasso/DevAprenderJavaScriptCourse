8a43bdb0915872cf2f00aafcc1a8559deab5330b
Rafaela Tasso <atassorafaela@gmail.com>
2020-09-16 11:56:37 -0300

    Class 35 - Combining Array

---
+++ classes/js/class35_combiningArray.js
@@ -0,0 +1,10 @@
+const numbers = [1,2,3,4,5,6];
+
+const addPoint = numbers.join('.');
+console.log(addPoint);
+
+const phrase = "Ola bem vindo ao curso";
+const result = phrase.split(' ');
+
+console.log(result);
+console.log(result.join('-'));
